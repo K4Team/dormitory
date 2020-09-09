f106cf5a018e83368ff90ba86bf4894f9b0df820
hujx <hujx@ultronera.com>
2020-09-09 17:47:36 +0800

    前端基本搭建

---
+++ config/defaultSettings.js
@@ -0,0 +1,18 @@
+
+const proSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#722ed1',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: '鑫遇 · 宿',
+  pwa: false,
+  iconfontUrl: '',
+};
+export default proSettings;
---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,58 @@
+import { getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Link, SelectLang, useIntl, connect } from 'umi';
+import React from 'react';
+import logo from '../assets/logo.svg';
+import styles from './UserLayout.less';
+
+const UserLayout = (props) => {
+  const {
+    route = {
+      routes: [],
+    },
+  } = props;
+  const { routes = [] } = route;
+  const {
+    children,
+    location = {
+      pathname: '',
+    },
+  } = props;
+  const { formatMessage } = useIntl();
+  const { breadcrumb } = getMenuData(routes);
+  const title = getPageTitle({
+    pathname: location.pathname,
+    formatMessage,
+    breadcrumb,
+    ...props,
+  });
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={title} />
+      </Helmet>
+
+      <div className={styles.container}>
+        <div className={styles.lang}>
+          <SelectLang />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <img alt="logo" className={styles.logo} src={logo} />
+                <span className={styles.title}>鑫遇 · 宿</span>
+              </Link>
+            </div>
+            <div className={styles.desc}>4305宿舍管理系统</div>
+          </div>
+          {children}
+        </div>
+        <div style={{textAlign:'center',width:'100%',color:'gray'}}><span>Copyright©2020 Powered by Hujingxin</span></div>
+      </div>
+    </HelmetProvider>
+  );
+};
+
+export default connect(({ settings }) => ({ ...settings }))(UserLayout);
